814fa25c8cb39ee2af4d6a94becf1c7464439ec8
Alexandre Choisy <alexandre.choisy@arengi.fr>
2021-06-02 11:06:37 +0200

    xliff v2 => Accept multiple columns un csv to xliff

--- xliff.php
+++ xliff.php
@@ -2,69 +2,119 @@
 /**
  * Created by PhpStorm.
  * User: alexandre
- * Date: 24/04/19
- * Time: 10:24
+ * Date: 02/06/2021
+ * Time: 10:22
  */
+
+include_once "./vendor/autoload.php";
 
 if (isset($_FILES['my_file'])) {
     if (($fh = fopen($_FILES['my_file']['tmp_name'], "r")) !== FALSE) {
 
         $arrayKeys = array();
+        $countLines = 0;
+        $languagesOrder = array();
         while (($line = fgetcsv($fh, 1000, ";")) !== FALSE) {
-            if($num = count($line) == 2) {
+            if($countLines === 0) {
+                // Récupération des en-têtes
+                foreach($line as $c => $headLabel) {
+                    if($c > 0) {
+                        $languagesOrder[] = strtolower($headLabel);
+                    }
+                }
+                $countLines++;
+                continue;
+            }
+            if($num = count($line) > 1) {
                 $key = $line[0];
-                $label = $line[1];
-                $arrayKeys[$key] = $label;
-            };
-
+                foreach($line as $c => $val) {
+                    if($c > 0) {
+                        $language = $languagesOrder[$c-1];
+                        $label = $val;
+                        $arrayKeys[$language][$key] = $label;
+                    }
+                }
+            }
+            $countLines++;
         }
         fclose($fh);
 
-        // 2) Export to xliff format
-        header("Content-type: application/xml");
-        header("Content-Disposition: attachment; filename=translations.xliff");
-        header("Pragma: no-cache");
-        header("Expires: 0");
+        $domain = $_POST['domain'];
 
-        $id = $_POST['my_idstart'];
-        foreach($arrayKeys as $key => $label) {
+        $zip = new ZipArchive();
+        $filename = sys_get_temp_dir()."/tmpziptranslator.zip";
 
-            $xliffTag = <<<EOT
-<trans-unit id="{$id}">
-    <source><![CDATA[{$key}]]></source>
-    <target><![CDATA[{$label}]]></target>
-</trans-unit>
-
-EOT;
-            echo $xliffTag;
-            $id++;
+        if(file_exists($filename)) {
+            unlink($filename);
         }
 
-        exit();
+        if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+            exit("Impossible d'ouvrir le fichier <$filename>\n");
+        }
+
+        foreach($arrayKeys as $locale => $catalog) {
+            $id = $_POST['my_idstart'];
+            $xmlTags = '';
+
+            foreach ($catalog as $key => $label) {
+
+                $xmlTags .= <<<EOL
+            <trans-unit id="$id" resname="$key">
+                <source><![CDATA[$key]]></source>
+                <target><![CDATA[$label]]></target>
+            </trans-unit>
+
+EOL;
+                $id++;
+            }
+
+            $xmlContent = <<<EOL
+<?xml version="1.0"?>
+<xliff version="1.2" xmlns="urn:oasis:names:tc:xliff:document:1.2">
+    <file source-language="fr" target-language="$locale" datatype="plaintext" original="file.ext">
+        <body>
+$xmlTags
+        </body>
+    </file>
+</xliff>
+
+EOL;
+            $zip->addFromString(sprintf('%s.%s.xliff', $domain, $locale), $xmlContent);
+        }
+
+        $zip->close();
+
+        header("Content-type: application/zip");
+        header("Content-Disposition: attachment; filename=translator.zip");
+        header("Pragma: no-cache");
+        header("Expires: 0");
+        readfile($filename);
+
     }
 }
 ?>
 
 <!doctype html>
 <html lang="fr">
-    <head>
-        <title>CSV To XLIFF</title>
-    </head>
-    <body>
-        <ul class="menu">
-            <li><a href="index.php">Xliff to CSV</a></li>
-        </ul>
+<head>
+    <title>CSV To XLIFF</title>
+</head>
+<body>
+<ul class="menu">
+    <li><a href="index.php">Xliff to CSV</a></li>
+</ul>
 
-        <br/><br/>
-        <hr>
-        <br/><br/>
+<br/><br/>
+<hr>
+<br/><br/>
 
-        <form method="post" enctype="multipart/form-data">
-            <label>Fichier csv : </label>
-            <input type="file" name="my_file" required>
-            <br/><br/>
-            <label>Id de début : </label><input type="text" name="my_idstart" required/>
-            <input type="submit" value="Upload">
-        </form>
-    </body>
+<form method="post" enctype="multipart/form-data">
+    <label>Fichier csv : </label>
+    <input type="file" name="my_file" required>
+    <br/><br/>
+    <label>Id de début : </label><input type="text" name="my_idstart" required/>
+    <label>Domaine : </label><input type="text" name="domain" required/>
+    <input type="submit" value="Upload">
+</form>
+</body>
 </html>
